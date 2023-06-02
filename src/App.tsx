3660f5cf6c2e88a54ac91c7211692fa9f7345892
Emilio Ramirez <eramirez0633@gmail.com>
2023-06-01 22:03:46 -0600

    complete absolute paths and folder structure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Navigator from 'navigation';
+
+function App(): JSX.Element {
+  return <Navigator />;
+}
+
+export default App;
---
+++ src/navigation/navigators/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from 'screens/Home';
+
+const Stack = createStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="home" component={Home} />
+    </Stack.Navigator>
+  );
+};
+
+export default RootNavigator;
